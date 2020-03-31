2b5c5ef3db3658fa88bc9e81e2b2f81aa223ef4a
Silvan Melchior <silvan.melchior@zuehlke.com>
2020-03-31 11:39:57 +0200

    simple react logic for later tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,43 @@ import logo from './logo.svg';
 import './App.css';
 
 
-//axios.post('/api/task/1/enter_code', {cc: 'asdf', code: 111}).then(response => console.log(response.data));
-axios.get('/api/task', {params: {cc: 'asdf'}}).then(response => console.log(response.data));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {tasks: []};
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  loadTasks = () => {
+    axios.get(
+      '/api/task',
+      {params: {cc: 'asdf'}}
+    )
+    .then(response => this.loadTaskDone(response.data));
+  };
+
+  loadTaskDone = (data) => {
+    this.setState({
+      tasks: data.tasks
+    })
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
+          {this.state.tasks.map(task => <div key={task.pk}>{task.title}</div>)}
+          {this.state.tasks.length === 0 &&
+            <button className="App-button" type="button" onClick={this.loadTasks}>Load Tasks</button>}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// Examples:
+// axios.post('/api/task/1/enter_code', {cc: 'asdf', code: 111}).then(response => console.log(response.data));
+// axios.get('/api/task', {params: {cc: 'asdf'}}).then(response => console.log(response.data));
