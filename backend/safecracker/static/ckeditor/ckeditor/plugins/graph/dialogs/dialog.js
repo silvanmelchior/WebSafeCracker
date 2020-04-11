dbfcb836e9c8deb538e6c3e980ca91bef2488f02
Silvan Melchior <silvan.melchior@zuehlke.com>
2020-04-11 13:26:02 +0200

    graphs

--- backend/safecracker/static/ckeditor/ckeditor/plugins/graph/dialogs/dialog.js
+++ backend/safecracker/static/ckeditor/ckeditor/plugins/graph/dialogs/dialog.js
@@ -2,7 +2,7 @@ CKEDITOR.dialog.add('graphDialog', function(editor) {
   return {
     title: 'Graph Properties',
     minWidth: 400,
-    minHeight: 150,
+    minHeight: 200,
     contents: [
       {
         id: 'tab-basic',
@@ -19,6 +19,12 @@ CKEDITOR.dialog.add('graphDialog', function(editor) {
             id: 'width',
             label: 'Width (e.g. in %)',
             validate: CKEDITOR.dialog.validate.notEmpty('Width field cannot be empty.')
+          },
+          {
+            type: 'text',
+            id: 'height',
+            label: 'Height (e.g. in %)',
+            validate: CKEDITOR.dialog.validate.notEmpty('Height field cannot be empty.')
           }
         ]
       }
@@ -27,11 +33,13 @@ CKEDITOR.dialog.add('graphDialog', function(editor) {
       let dialog = this;
       let filename = dialog.getValueOf('tab-basic', 'filename');
       let width = dialog.getValueOf('tab-basic', 'width');
+      let height = dialog.getValueOf('tab-basic', 'height');
 
       let el = editor.document.createElement('div');
       el.setStyle('width', width);
       el.setStyle('margin-left', 'auto');
       el.setStyle('margin-right', 'auto');
+      el.setStyle('padding-bottom', height);
       el.setStyle('border', '1px black solid');
       el.setAttribute('data-graphurl', filename);
       el.setText('Graph: ' + filename);
--- frontend/src/Task.js
+++ frontend/src/Task.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Prism from "prismjs";
+import Plotly from 'plotly.js-basic-dist';
 import 'prismjs/plugins/line-numbers/prism-line-numbers'
 import 'prismjs/themes/prism-okaidia.css'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 import "./Task.css";
+import axios from "axios";
 
 
 class Task extends React.Component {
@@ -16,14 +18,65 @@ class Task extends React.Component {
     Prism.highlightAll();
     // Image
     [...document.querySelectorAll('[data-imgurl]')].forEach(div => {
+      div.innerHTML = 'Loading...';
       let path = div.dataset.imgurl;
       let img = document.createElement('img');
+      img.onload = () => {
+        div.innerHTML = '';
+        div.style.border = '';
+        div.className = 'Task-blockimg';
+        div.appendChild(img);
+      };
       img.src = this.props.task.media_url + path;
-      div.innerHTML = '';
-      div.style.border = '';
-      div.className = 'Task-blockimg';
-      div.appendChild(img);
-    })
+    });
+    // Graph
+    [...document.querySelectorAll('[data-graphurl]')].forEach(div => {
+      div.innerHTML = 'Loading...';
+      let path = div.dataset.graphurl;
+
+      axios.get(this.props.task.media_url + path).then(response => {
+        let inner_div = document.createElement('div');
+        inner_div.style.width = '100%';
+        inner_div.style.height = '100%';
+        inner_div.style.position = 'absolute';
+        inner_div.style.top = '0';
+        inner_div.style.left = '0';
+
+        div.innerHTML = '';
+        div.style.position = 'relative';
+        div.style.border = '';
+        div.appendChild(inner_div);
+
+        let graph_data = response.data;
+        let traces = [];
+        for(const key in graph_data.signals) {
+          let trace = {
+            x: graph_data.signals[key][0],
+            y: graph_data.signals[key][1],
+            mode: 'lines',
+            name: key
+          };
+          traces.push(trace);
+        }
+        let layout = {
+          showlegend: true,
+          margin: {t: 0, l: 50, r: 0, b: 40},
+          grid: true,
+          xaxis: {
+            title: 'Time [s]',
+            range: [graph_data.limits.xmin, graph_data.limits.xmax]
+          },
+          yaxis: {
+            title: 'Voltage [V]',
+            range: [graph_data.limits.ymin, graph_data.limits.ymax]
+          },
+        };
+        let config = {
+          displayModeBar: false
+        };
+        Plotly.newPlot(inner_div, traces, layout, config);
+      });
+    });
   }
 
   render() {
