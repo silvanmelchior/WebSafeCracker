1e7abc5d86979e1e2e8f2611a37853608378279f
Silvan Melchior <silvan.melchior@zuehlke.com>
2020-04-11 11:44:57 +0200

    ckeditor dialogues width

--- backend/safecracker/static/ckeditor/ckeditor/plugins/authimage/dialogs/dialog.js
+++ backend/safecracker/static/ckeditor/ckeditor/plugins/authimage/dialogs/dialog.js
@@ -2,7 +2,7 @@ CKEDITOR.dialog.add('authimageDialog', function(editor) {
   return {
     title: 'Image Properties',
     minWidth: 400,
-    minHeight: 100,
+    minHeight: 150,
     contents: [
       {
         id: 'tab-basic',
@@ -12,7 +12,13 @@ CKEDITOR.dialog.add('authimageDialog', function(editor) {
             type: 'text',
             id: 'filename',
             label: 'File Name',
-            validate: CKEDITOR.dialog.validate.notEmpty('Field cannot be empty.')
+            validate: CKEDITOR.dialog.validate.notEmpty('File name field cannot be empty.')
+          },
+          {
+            type: 'text',
+            id: 'width',
+            label: 'Width (e.g. in %)',
+            validate: CKEDITOR.dialog.validate.notEmpty('Width field cannot be empty.')
           },
           {
             type: 'checkbox',
@@ -25,20 +31,22 @@ CKEDITOR.dialog.add('authimageDialog', function(editor) {
     onOk: function() {
       let dialog = this;
       let filename = dialog.getValueOf('tab-basic', 'filename');
+      let width = dialog.getValueOf('tab-basic', 'width');
       let inline = dialog.getValueOf('tab-basic', 'inline');
 
       let el = null;
       if(inline) {
         el = editor.document.createElement('span');
         el.setStyle('display', 'inline-block');
-        el.setStyle('width', '20%');
+        el.setStyle('width', width);
       }
       else {
         el = editor.document.createElement('div');
-        el.setStyle('width', '100%');
+        el.setStyle('width', width);
+        el.setStyle('margin-left', 'auto');
+        el.setStyle('margin-right', 'auto');
       }
       el.setAttribute('data-imgurl', filename);
-      el.setText('Image: ' + filename);
       el.setText('Image: ' + filename);
       el.setStyle('border', '1px black solid');
 
--- backend/safecracker/static/ckeditor/ckeditor/plugins/graph/dialogs/dialog.js
+++ backend/safecracker/static/ckeditor/ckeditor/plugins/graph/dialogs/dialog.js
@@ -2,7 +2,7 @@ CKEDITOR.dialog.add('graphDialog', function(editor) {
   return {
     title: 'Graph Properties',
     minWidth: 400,
-    minHeight: 100,
+    minHeight: 150,
     contents: [
       {
         id: 'tab-basic',
@@ -12,7 +12,13 @@ CKEDITOR.dialog.add('graphDialog', function(editor) {
             type: 'text',
             id: 'filename',
             label: 'File Name',
-            validate: CKEDITOR.dialog.validate.notEmpty('Field cannot be empty.')
+            validate: CKEDITOR.dialog.validate.notEmpty('File name field cannot be empty.')
+          },
+          {
+            type: 'text',
+            id: 'width',
+            label: 'Width (e.g. in %)',
+            validate: CKEDITOR.dialog.validate.notEmpty('Width field cannot be empty.')
           }
         ]
       }
@@ -20,9 +26,14 @@ CKEDITOR.dialog.add('graphDialog', function(editor) {
     onOk: function() {
       let dialog = this;
       let filename = dialog.getValueOf('tab-basic', 'filename');
+      let width = dialog.getValueOf('tab-basic', 'width');
 
       let el = editor.document.createElement('div');
-      el.setAttribute('data-graph', filename);
+      el.setStyle('width', width);
+      el.setStyle('margin-left', 'auto');
+      el.setStyle('margin-right', 'auto');
+      el.setStyle('border', '1px black solid');
+      el.setAttribute('data-graphurl', filename);
       el.setText('Graph: ' + filename);
 
       editor.insertElement(el);
