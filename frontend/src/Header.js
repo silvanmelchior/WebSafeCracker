6cbbbd08cb820da963b47b728ba5ad26e67653be
Silvan Melchior <silvan.melchior@zuehlke.com>
2020-04-11 15:11:51 +0200

    header styling

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "./Header.css";
+import logo from './logo.png'
+
+
+function remaining_time(t) {
+  let mins = Math.ceil(t/60);
+  if (mins === 1) return '1 minute';
+  else return mins + ' minutes';
+}
 
 
 function Header (props) {
   return (
     <div className="Header">
-      <div className="Header-title">Web Safe Cracker</div>
+      <div className="Header-logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="Header-title"><div>Web Safe Cracker</div></div>
       <div className="Header-status">
-        Name: {props.competitor_name}<br/>
-        Time: {props.remaining_time}<br/>
-        Points: {props.points}
+        <div className="Header-status-name">{props.competitor_name}</div>
+        <div className="Header-status-time">Remaining Time: {remaining_time(props.remaining_time)}</div>
+        <div className="Header-status-points">Your Points: {props.points}</div>
       </div>
     </div>
   )
