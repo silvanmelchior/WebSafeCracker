8582533c91e9e63d34f72644b86a96021b1c2e9d
Silvan Melchior <silvan.melchior@zuehlke.com>
2020-04-13 14:46:36 +0200

    config in env variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,16 +16,13 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+# Dev/Prod Settings
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1=wb#@#0&6jjpga*ayygy271bbtjyrddjfb4#!orh@jr$i-$!*'
+SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG'] == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
 
 
 # Application definition
